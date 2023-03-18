fe78a8e46b974f5ba36112d86d131647067bc0da
Shenghao Dong <shenghaodong18@gmail.com>
2023-03-17 23:27:27 -0400

    added basic idea for how to code pathToFreedom

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -2,8 +2,33 @@
 
 bool isPathToFreedom(MazeCell* start, const std::string& moves) {
     /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
-    return false;
-    //Maybe use recursion since I can go back onto spaces I've been to already
+    // (void) start;
+    // (void) moves;
+    // return false;
+
+    //Set the bool to true if you find the item in the maze
+    bool spellBook = false;
+    bool potion = false;
+    bool wand = false;
+
+
+    //Go through every letter in moves and if any leads to nullptr then break and return false
+
+    //In the if statement check if it leads to nullptr if it does then return false
+    for(int i = 0; i < moves.length(); i++){
+        if(moves.substr(i) =="N"){
+            start -> north; 
+        }
+        if(moves.substr(i) =="E"){
+            start -> east; 
+            
+        }
+        if(moves.substr(i) =="S"){
+            start -> south;
+            
+        }
+        if(moves.substr(i) =="W"){
+            start -> west;
+        }
+    }
 }
